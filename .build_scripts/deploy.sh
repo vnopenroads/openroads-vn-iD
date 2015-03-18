4b354009c6942eb58eb8d72d4e4f9361fbf1a102
Joe Flasher <joseph.flasher@gmail.com>
2015-03-18 08:38:07 -0400

    Using Travis to deploy to gh-pages
    Should be most of the way towards #14

---
+++ .build_scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+# If this is the deploy branch, push it up to gh-pages
+if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
+  echo "Get ready, we're pushing to gh-pages!"
+  mkdir dist
+  cp -r index.html css data js svg dist
+  cd dist
+  git init
+  git config user.name "Travis-CI"
+  git config user.email "travis@somewhere.com"
+  git add .
+  git commit -m "CI deploy to gh-pages"
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+  rm -rf .git/
+else
+  echo "Not a publishable branch so we're all done here"
+fi
