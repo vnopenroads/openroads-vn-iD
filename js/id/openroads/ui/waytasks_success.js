1903d608e2edee560b4aac7423e077153381485c
Daniel da Silva <daniel@developmentseed.org>
2016-04-04 17:45:28 +0100

    Temporarily hide "next task" button

--- js/id/openroads/ui/waytasks_success.js
+++ js/id/openroads/ui/waytasks_success.js
@@ -15,24 +15,24 @@ iD.openroads.ui.WayTasksSuccess = function(context) {
                 }
             });
 
-        selection.append('button')
-            .attr('class', 'bttn bttn-next')
-            .text(t('waytasks.next_task'))
-            .on('click', function() {
-                // TODO:
-                // - Figure out what's the next way.
-                //
-                var wayid = 13900
-                context.connection().loadEntity('w' + wayid, function(err, entity) {
-                    if (err) {
-                        context.enter(iD.modes.Browse(context));
-                        return;
-                    }
-                    context.map().zoomTo(entity);
-                    context.enter(iD.modes.Select(context, [entity.id])
-                        .suppressMenu(true));
-                })
-            });
+        // selection.append('button')
+        //     .attr('class', 'bttn bttn-next')
+        //     .text(t('waytasks.next_task'))
+        //     .on('click', function() {
+        //         // TODO:
+        //         // - Figure out what's the next way.
+        //         //
+        //         var wayid = 13900
+        //         context.connection().loadEntity('w' + wayid, function(err, entity) {
+        //             if (err) {
+        //                 context.enter(iD.modes.Browse(context));
+        //                 return;
+        //             }
+        //             context.map().zoomTo(entity);
+        //             context.enter(iD.modes.Select(context, [entity.id])
+        //                 .suppressMenu(true));
+        //         })
+        //     });
     }
 
     return wayTasksSuccess;
